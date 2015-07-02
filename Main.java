9c9c11036997569383216f33575e302a3297ff14
pavankvvd <pavankvvd@users.noreply.github.com>
2015-07-02 00:29:43 -0700

    Changes

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    static final List<String> tokens = Arrays.asList(new String[]{"CREATE",
+    static final List<String> keywords = Arrays.asList(new String[]{"CREATE",
         "TABLE", "INSERT", "SELECT", "INTO", "FROM", "WHERE", "*", "(", ")",
                  "MDSYS.SPATIAL_INDEX", "INDEXTYPE", "PRIMARY KEY",
                  "REFERENCES", "NULL", "NOT", "LIKE", "IN", "BETWEEN", "OR", "AND",
@@ -15,20 +15,16 @@ public class Main {
                  "BIGINT", "DECIMAL", "NUMERIC", "FLOAT", "REAL", "TIME", "XML",
                  "SDO_ELEM_INFO_ARRAY", "SDO_ORDINATE_ARRAY" }
 
-    
-    
-    public enum State {
+   public enum State {
         CREATE, 
         TABLE, 
-        TABLE_NAME,  
-        ATTRIBUTE_NAME,
+        NAME,  
         ATTRIBUTE_TYPE,
-        VALUE,
         ENDED
-    }
+   }
 
-    public enum Event {
-        START;
+    public enum Event{
+        SPACE(' '),
         COMMA(','),
         OPEN_BRACE('('),
         CLOSEBRACE(')'),
@@ -49,16 +45,28 @@ public class Main {
                 input += inputStr[i++];
             }   
             
+            switch(input){
+                    case "CREATE": currentState = State.CREATE:
+                                   break;
+                    case  "TABLE": currentState = State.TABLE:
+                                   break;
+                    default : if(input.length>0 and dataTypes.contains(input))
+                                    currentState = State.ATTRIBUTE_TYPE;
+                              else  currentState = State.NAME;  
+                              break;
+            }
+            
             switch(inputStr[i]){
-                case ' ': 
+                case ' ': setCurrentState(State.ATTRIBUTE_NAME, State.TABLE, State.TABLE_NAME, State.);
+                          break;
                 case ',': setCurrentState(State.ATTRIBUTE_NAME); 
-                        break;
+                          break;
                 case '(': setCurrentState(State.ATTRIBUTE_NAME);
-                        break;
+                          break;
                 case ')': setCurrentState(State.ENDED);
-                        break;
+                          break;
                 default : System.out.println("Syntax Error at ith positiion");
-                        break;
+                          break;
             }   
         }
     }
